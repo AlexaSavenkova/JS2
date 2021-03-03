73fa48fe7d5bf0557584d8ee10fcec65e6add78d
Alexandra <sav_alexandra@yahoo.com>
2021-03-03 16:17:52 +0100

    webpack developer mode

---
+++ src/public/js/ErrorComp.js
@@ -0,0 +1,25 @@
+const error = {
+    data() {
+       return {
+           text:'',
+           isVisible: false,
+       }
+    },
+    methods: {
+        showErrorMsg(error) {
+          this.text = error;
+          this.isVisible = true;
+        },
+
+        closeErrorMsg() {
+            this.isVisible = false;
+        }
+    },
+    template:
+        `<div class="errorBlock" v-if="isVisible">
+            <p>{{text}} </p>
+            <button @click= "closeErrorMsg()">Close</button>
+        </div> `
+};
+
+export default error;
---
+++ webpack.dev.config.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+    entry: {
+        main: ["@babel/polyfill", "./src/public/index.js"]
+    },
+    output: {
+        path: path.join(__dirname, 'dist/public'),
+        publicPath: "/",
+        filename: "js/[name].js"
+    },
+    target: 'web',
+    devtool: "#source-map",
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "html-loader"
+                    }
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|jpg|svg|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: 'images/[name].[ext]'
+                        }
+                    }
+                ]
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/public/index.html',
+            filename: 'index.html',
+            excludeChunks: ['server']
+        }),
+        new CopyPlugin([
+            {
+                from: 'src/public/images/catalog',
+                to: 'images/catalog/[name].[ext]',
+                toType: 'template'
+            }
+        ])
+    ]
+};
